2685c38a98ebe17e484ecfe161a027155d96dc29
Yaroslav <107879647+YaroslavYanko@users.noreply.github.com>
2023-01-02 14:48:22 +0200

    Add files via upload

---
+++ seeds/default/1671114239650_goodsSeed.sql
@@ -0,0 +1,6 @@
+SET check_function_bodies = false;
+INSERT INTO public.goods (id, image, "shortName", title, price, description) VALUES ('be6f120e-9a3d-45a9-9662-592c93f77dbb', '1.jpe', 'Коротка назва', '"Головна назва', 280, 'Детальний опис товару');
+INSERT INTO public.goods (id, image, "shortName", title, price, description) VALUES ('cf054fa7-6fb1-4f2e-b5d9-b6c5c4b98dd4', '2.jpeg', 'Коротка назва', 'Головна назва', 280, 'Детальний опис товару');
+INSERT INTO public.goods (id, image, "shortName", title, price, description) VALUES ('a6800aa8-948c-42d5-b9c5-2003e6da91ba', '3.jpeg', 'Коротка назва', 'Головна назва', 350, 'Детальний опис товару');
+INSERT INTO public.goods (id, image, "shortName", title, price, description) VALUES ('36178a43-8d80-4e6b-8558-3eb93b78f6ad', '5.jpeg', 'Коротка назва', 'Головна назва', 900, 'Детальний опис товару');
+INSERT INTO public.goods (id, image, "shortName", title, price, description) VALUES ('93e941fd-1aa4-4f7f-aaee-97f4bbbfcdf0', '4.jpeg', 'Коротка назва', 'Головна назва', 900, 'Детальний опис товару');
