e8e45b1516e7dc5e9d34fe01b9eac43ed8d3498b
Yaroslav <107879647+YaroslavYanko@users.noreply.github.com>
2023-02-07 13:27:38 +0200

    migrations user

---
+++ migrations/default/1675758856502_added_amount_to_total_calculation/down.sql
@@ -0,0 +1,9 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION sum_by_order(orders_row orders)
+-- RETURNS NUMERIC AS $$
+--   SELECT SUM(p.price * om.amount)
+--   FROM orders_products as om
+--   JOIN goods as p ON om.products_id=p.id
+--   WHERE order_id=orders_row.id
+-- $$ LANGUAGE sql STABLE;
